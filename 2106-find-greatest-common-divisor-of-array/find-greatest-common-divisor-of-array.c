001f71a5b034698860cd2bd174534e68e2d4c043
MAINAK KUNDU <mainakkundu13@gmail.com>
2026-07-18 09:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (62.69%) - LeetSync

---
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.c
@@ -0,0 +1,18 @@
+int findGCD(int* nums, int numsSize) {
+    int gcd(int a, int b) {
+        if(b==0)
+            return a;
+       return gcd(b,a%b);     
+    }
+    int min = nums[0];
+    int max = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] < min) {
+            min = nums[i];
+        }
+        if (nums[i] > max) {
+            max = nums[i];
+        }
+    }
+    return gcd(min, max);
+}
